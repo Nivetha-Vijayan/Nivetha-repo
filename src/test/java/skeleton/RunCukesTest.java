e1f6da67e97258e601e716bab4783c1c86ba06f0
Nivedhitha Murugesan <nivedhitha.murugesan@accenture.com>
2019-09-26 11:45:06 +0530

    updated cucumber files

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(features="C:\\Test\\Nivetha-repo\\jenkins-workspace\\cucumber-skeleton-project-4.0\\cucumber-skeleton-project-4.0\\src\\test\\resources\\skeleton\\Login.feature",
+plugin="json: target/cucumberReport.json")
 public class RunCukesTest {
 }
